7e48ec02ed7ea8dfe71e6b46a217e763c5658bcd
JuanCamiloGutiV <144349089+JuanCamiloGutiV@users.noreply.github.com>
2024-06-17 10:53:16 -0500

    Add files via upload
    funciones "itinerarios" e "itinerariosEscalas" implementadas.

--- app/src/main/scala/proyecto/Iterinario.scala
+++ app/src/main/scala/proyecto/Iterinario.scala
@@ -5,8 +5,31 @@ class Itinerario() {
   type aeropuertos = List[Aeropuerto]
   type vuelos = List[Vuelo]
 
-  def itinerarios(vuelos: List[Vuelo], aeropuertos:List[Aeropuerto]): (String, String) => List[Itinerario] = {
-    (cod1:String, cod2:String)=> List[Itinerario]()
+  def itinerarios(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+    def buscarItinerarios(cod1: String, cod2: String, visitados: Set[String]): List[List[Vuelo]] = {
+      if (cod1 == cod2) {
+        List(List())
+      } else {
+        vuelos.filter(_.Org == cod1).flatMap { vuelo =>
+          if (!visitados(vuelo.Dst)) {
+            val nuevosVisitados = visitados + vuelo.Dst
+            val itinerariosRestantes = buscarItinerarios(vuelo.Dst, cod2, nuevosVisitados)
+            itinerariosRestantes.map(vuelo :: _)
+          } else {
+            Nil
+          }
+        }
+      }
+    }
+
+    def construirItinerarios(vuelos: List[Vuelo]): List[Itinerario] = {
+      vuelos.map(vuelo => Itinerario(List(vuelo)))
+    }
+
+    (cod1: String, cod2: String) => {
+      val posiblesItinerarios = buscarItinerarios(cod1, cod2, Set())
+      posiblesItinerarios.map(construirItinerarios)
+    }
   }
 
   def itinerariosTiempo(vuelos: List[Vuelo], aeropuertos:List[Aeropuerto]): (String, String) => List[Itinerario] = {
@@ -50,13 +73,26 @@ class Itinerario() {
     (cod1: String, cod2: String) => buscarVuelos(cod1, cod2)
   }
 
-  def itinerariosEscalas(vuelos:List[Vuelo], aeropuertos:List[Aeropuerto]):(String, String)=>List[Itinerario]
-  = {
-    //Recibe una lista de vuelos y aeropuertos
-    //Retorna una función que recibe los codigos de dos aeropuertos
-    //Retorna todos los tres mejores itinerarios posibles de cod1 a cod2
-    //que minimizan el número de escalas
-    (cod1:String, cod2:String)=> List[Itinerario]()
+  def itinerariosEscalas(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[List[Vuelo]] = {
+    // fun aux para hallar los itinerarios con menor escalas
+    def encontrarItinerarios(cod1: String, cod2: String, visitados: Set[String], acumulado: List[Vuelo]): List[List[Vuelo]] = {
+      if (cod1 == cod2) {
+        List(acumulado.reverse)
+      } else {
+        val vuelosSalientes = vuelos.filter(v => v.Org == cod1 && !visitados(v.Dst))
+
+        vuelosSalientes.flatMap { vuelo =>
+          val nuevosVisitados = visitados + cod1
+          val nuevosAcumulado = vuelo :: acumulado
+          encontrarItinerarios(vuelo.Dst, cod2, nuevosVisitados, nuevosAcumulado)
+        }
+      }
+    }
+
+    (cod1: String, cod2: String) => {
+      val itinerarios = encontrarItinerarios(cod1, cod2, Set(), List())
+      itinerarios.sortBy(_.length).take(3)
+    }
   }
 
   def itinerariosAire(vuelos: List[Vuelo], aeropuertos:List[Aeropuerto]): (String, String) => List[Itinerario] = {
