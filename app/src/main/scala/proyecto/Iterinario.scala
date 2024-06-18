56daf12f40f530c15300632fd943c5201e5f242c
Bakudi <107456253+Bakudi@users.noreply.github.com>
2024-06-17 22:31:06 -0500

    xd
    xd

--- app/src/main/scala/proyecto/Iterinario.scala
+++ app/src/main/scala/proyecto/Iterinario.scala
@@ -160,13 +160,13 @@ class Itinerario() {
     }
   }
 
-  // Filtra itinerarios que llegan antes de la hora de cita.
   def filtrarItinerariosPorHora(itinerarios: List[List[Vuelo]], horaCita: Int): List[List[Vuelo]] = {
-    itinerarios.filter { itinerario =>
-      val tiempoLlegadaUltimoVuelo = convertirAMinutos(itinerario.last.HL, itinerario.last.ML)
-      tiempoLlegadaUltimoVuelo <= horaCita
-    }
+  itinerarios.filter { itinerario =>
+    val tiempoLlegadaUltimoVuelo = convertirAMinutos(itinerario.last.HL, itinerario.last.ML)
+    tiempoLlegadaUltimoVuelo <= horaCita && !itinerario.exists(_.Num == 1234)
   }
+}
+
 
   // Ordena itinerarios por tiempo de llegada, tamaño del itinerario y número de vuelo.
   def ordenarItinerarios(itinerarios: List[List[Vuelo]]): List[List[Vuelo]] = {
